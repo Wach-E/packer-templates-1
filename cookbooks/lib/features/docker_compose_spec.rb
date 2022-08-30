1fbc073238e112ca8bdb130616b02fb138148758
Konrad Wrzos <konrad@travis-ci.org>
2022-08-30 17:38:15 +0200

    Updating docker

--- cookbooks/lib/features/docker_compose_spec.rb
+++ cookbooks/lib/features/docker_compose_spec.rb
@@ -3,6 +3,6 @@
 describe 'docker-compose installation' do
   describe command('docker-compose --version') do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match(/^docker-compose version:?\s+\d+\.\d+\.\d+/) }
+    its(:stdout) { should match(/^docker-compose version:?\s+v\d+\.\d+\.\d+/) }
   end
 end
