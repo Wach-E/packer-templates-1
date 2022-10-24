348127de2c06d2e9fa8cd31a5008eeb1a4ec1369
Konrad Wrzos <konrad@travis-ci.org>
2022-10-24 16:03:21 +0200

    Testing permissions

--- lxd/scripts/go.sh
+++ lxd/scripts/go.sh
@@ -14,6 +14,7 @@ __install_gimme() {
   curl -sL -o $HOME/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
   chmod +x $HOME/bin/gimme
   sudo ls -al /tmp/
+  sudo chown -R travis: /tmp/__common-lib.sh
 }
 
 main "$@"
